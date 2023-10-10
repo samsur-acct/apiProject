fa34ace42feee0b60cbfb3995f451ec1a605ed6f
samsur-acct <samsur.acct@gmail.com>
2023-10-10 13:31:59 -0400

    refactored

--- src/main/java/com/personal/ApiClass.java
+++ src/main/java/com/personal/ApiClass.java
@@ -1,18 +1,75 @@
 package com.personal;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.personal.response.BookingDetails;
+import com.personal.response.Users;
+import com.personal.response.UsersClass;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class ApiClass {
 
+    
+
     public Response getBookingDetailsById(String bookingId) {
         RequestSpecification request = RestAssured.given();
         Response response = request.baseUri("https://restful-booker.herokuapp.com")
-        .when().log().all()
-        .get(String.format("/booking/%s",bookingId));
+                .when().log().all()
+                .get(String.format("/booking/%s", bookingId));
         return response;
+    }
 
+    public Response getBookingDetailsById2(String bookingId) {
+        RequestSpecification request = RestAssured.given();
+        Response response = request.baseUri("https://restful-booker.herokuapp.com")
+                .when().log().all()
+                .get(String.format("/booking/%s", bookingId));
+        return response;
+    }
+
+    public Response getBookingDetails() {
+        RequestSpecification request = RestAssured.given();
+        Response response = request.baseUri("https://restful-booker.herokuapp.com")
+                .when().log().all()
+                .get("/booking");
+        return response;
+    }
+
+    public List<BookingDetails> getBookingDetailsObject(String id) {
+
+        var details = Arrays.asList(getBookingDetailsById2(id).body().as(BookingDetails[].class));
+        return details;
+    }
+
+    public Response getUsersById(int userId) {
+        RequestSpecification request = RestAssured.given();
+        Response response = request.baseUri("https://jsonplaceholder.typicode.com")
+                .when().log().all()
+                .get(String.format("/posts/%s", userId));
+        return response;
+    }
+
+    public UsersClass getUsersByIdObject(int id) throws JsonMappingException, JsonProcessingException {
+
+        var response = getUsersById(id);
+        if(response.getStatusCode() == 200){
+            response.prettyPrint();
+            // ObjectMapper objectMapper = new ObjectMapper();
+            // return objectMapper.readValue(response.getBody().asString(), Users.class);
+            return response.body().as(UsersClass.class);
+
+
+
+
+        }else return null;
+        
     }
 
 }
--- src/test/java/com/personal/ApiTest2.java
+++ src/test/java/com/personal/ApiTest2.java
@@ -1,16 +1,19 @@
 package com.personal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
 import io.restassured.response.Response;
 
-public class ApiTest {
+public class ApiTest2 {
 
     ApiClass apiClass = new ApiClass();
 
     @ParameterizedTest
-    @ValueSource(strings = { "295" })
+    @ValueSource(strings = { "587" })
     public void testApiGet_validaData_200Response(String bookingId) throws InterruptedException {
         Response response = apiClass.getBookingDetailsById(bookingId);
         response.prettyPrint();
@@ -19,12 +22,27 @@ public class ApiTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "295"})
+    @ValueSource(strings = { "483" })
     public void testApiGet_validaData_200Response2(String bookingId) throws InterruptedException {
         Response response = apiClass.getBookingDetailsById(bookingId);
         response.prettyPrint();
         assertEquals(200, response.getStatusCode(), "Status Code didn't match");
         Thread.sleep(2000);
     }
+
+    @Test
+    public void testApiGet_validaData_200R() {
+        Response response = apiClass.getBookingDetails();
+        response.prettyPrint();
+        assertEquals(200, response.getStatusCode(), "Status Code didn't match");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1 })
+    public void testGetUserById_validaData_200Response(int userid) throws InterruptedException {
+        Response response = apiClass.getUsersById(userid);
+        response.prettyPrint();
+    }
+
 
 }
