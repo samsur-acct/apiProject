dbe2ee4b56184d356a0f94fc61a42f6b3b6dae1e
samsur-acct <samsur.acct@gmail.com>
2023-10-27 16:17:33 -0400

    add callback example

--- src/main/java/com/personal/ApiClass.java
+++ src/main/java/com/personal/ApiClass.java
@@ -1,11 +1,5 @@
 package com.personal;
 
-import java.util.Arrays;
-import java.util.List;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
---
+++ src/main/java/com/personal/javaTopic/callback/Callback.java
@@ -0,0 +1,5 @@
+package com.personal.javaTopic.callback;
+
+interface Callback {
+    void onComplete();
+}
